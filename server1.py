6760347dea377659afef77fda537c467d9303375
RoboLearnAI47 <140956306+codesFolder@users.noreply.github.com>
2025-09-07 01:35:21 +0530

    Update server1.py

--- server1.py
+++ server1.py
@@ -1,16 +1,4 @@
-# -*- coding: utf-8 -*-
 # ba_meta require api 9
-"""
-Server Switch Plugin by My.Smoothy
-Lets you switch recently joined servers very quickly
-+ Added button to quickly look into public server list without leaving current game.
-
-discord: mr.smoothy
-https://discord.gg/ucyaesh 
-Youtube : Hey Smoothy
-Download more mods from 
-https://bombsquad-community.web.app/mods
-"""
 import babase
 import bauiv1lib.mainmenu as bastd_ui_mainmenu
 import bauiv1 as bui
@@ -27,12 +15,10 @@ def _refresh_in_game(func):
         uiscale = bui.app.ui_v1.uiscale
         bui.containerwidget(
             edit=self._root_widget,
-            size=(self._width * 2, self._height),  # double the width
+            size=(self._width * 2, self._height),
             scale=(
-                2.15
-                if uiscale is bui.UIScale.SMALL
-                else 1.6
-                if uiscale is bui.UIScale.MEDIUM
+                2.15 if uiscale is bui.UIScale.SMALL
+                else 1.6 if uiscale is bui.UIScale.MEDIUM
                 else 1.0
             ),
         )
@@ -51,7 +37,7 @@ def _refresh_in_game(func):
         self._public_servers = bui.buttonwidget(
             color=(0.8, 0.45, 1),
             parent=self._root_widget,
-            position=(h + self._button_width - 10, v + 60 + 20),
+            position=(h + self._button_width - 10, v + 80),
             size=(self._button_width / 4, self._button_height / 2),
             scale=1.0,
             autoselect=self._use_autoselect,
@@ -59,43 +45,43 @@ def _refresh_in_game(func):
             on_activate_call=bs.Call(public_servers)
         )
         for server in servers:
-            self._server_button = bui.buttonwidget(
+            bui.buttonwidget(
                 color=(0.8, 0, 1),
                 parent=self._root_widget,
                 position=((h - self._button_width / 2) + self._button_width + 20, v),
                 size=(self._button_width, self._button_height),
                 scale=1.0,
                 autoselect=self._use_autoselect,
-                label=server["name"][0:22],
+                label=server["name"][:22],
                 on_activate_call=bs.Call(bs.connect_to_party, server["ip"], server["port"])
             )
             v -= 50
-
         return returnValue
     return wrapper
 
 
-connect = bs.connect_to_party
+_original_connect = bs.connect_to_party
 
 
 def connect_to_party(address, port=43210, print_progress=False):
     global current_server_ip, current_server_port
-    if bs.get_connection_to_host_info() != {}:
+    if bs.get_connection_to_host_info():
         bs.disconnect_from_host()
     current_server_ip = address
     current_server_port = port
-    connect(address, port, print_progress)
+    _original_connect(address, port, print_progress)
     babase.apptimer(1, check_connect_status)
 
 
 def check_connect_status():
     global servers, current_server_ip, current_server_port
-    if bs.get_connection_to_host_info() != {}:
-        if not bs.get_connection_to_host_info()['name']:
+    info = bs.get_connection_to_host_info()
+    if info:
+        if not info.get('name'):
             babase.apptimer(1, check_connect_status)
             return
         new_server = {
-            "name": bs.get_connection_to_host_info()['name'],
+            "name": info['name'],
             "ip": current_server_ip,
             "port": current_server_port
         }
@@ -103,7 +89,7 @@ def check_connect_status():
             servers.append(new_server)
             servers = servers[-3:]
     else:
-        print("connection failed, falling back to gather window")
+        print("Connection failed, opening public servers list...")
         public_servers()
 
 
