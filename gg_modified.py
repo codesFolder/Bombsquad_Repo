389913414e2469d4a5c7d0cc74af628234ea73a1
RoboLearnAI47 <140956306+codesFolder@users.noreply.github.com>
2025-09-07 01:05:05 +0530

    Update gg_modified.py

--- gg_modified.py
+++ gg_modified.py
@@ -39,10 +39,13 @@ gg_msgs = [
     "Well fought!"
 ]
 
-class PartyWindowWithTwoButtons(bauiv1lib.party.PartyWindow):
+class PartyWindowWithTwoIndependentCooldowns(bauiv1lib.party.PartyWindow):
     def __init__(s, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        s._delay = s._a = 50  # cooldown shared for both buttons
+
+        # Separate cooldowns
+        s._delay_sorry = s._a_sorry = 50
+        s._delay_gg = s._a_gg = 50
 
         # Sorry button (original position)
         s._btn_sorry = bui.buttonwidget(
@@ -55,7 +58,7 @@ class PartyWindowWithTwoButtons(bauiv1lib.party.PartyWindow):
             on_activate_call=s._apologize
         )
 
-        # GG button (shifted left by ~60px)
+        # GG button (shifted left)
         s._btn_gg = bui.buttonwidget(
             parent=s._root_widget,
             size=(50, 35),
@@ -66,32 +69,37 @@ class PartyWindowWithTwoButtons(bauiv1lib.party.PartyWindow):
             on_activate_call=s._send_gg
         )
 
-    def _ok(s, a):
+    # Update Sorry button label
+    def _ok_sorry(s, a):
         if s._btn_sorry.exists():
             bui.buttonwidget(edit=s._btn_sorry,
-                             label=str((s._delay - a) / 10) if a != s._delay else 'Sorry')
+                             label=str((s._delay_sorry - a) / 10) if a != s._delay_sorry else 'Sorry')
+            s._a_sorry = a
+
+    # Update GG button label
+    def _ok_gg(s, a):
         if s._btn_gg.exists():
             bui.buttonwidget(edit=s._btn_gg,
-                             label=str((s._delay - a) / 10) if a != s._delay else 'GG')
-        s._a = a
+                             label=str((s._delay_gg - a) / 10) if a != s._delay_gg else 'GG')
+            s._a_gg = a
 
     def _apologize(s):
-        if s._a != s._delay:
+        if s._a_sorry != s._delay_sorry:
             push("Too fast!")
             return
         bs.chatmessage(random.choice(lmao).replace('%', random.choice(sory)).replace('$', random.choice(cash)))
-        for i in range(10, s._delay + 1):
-            bs.apptimer((i - 10) / 10, bs.Call(s._ok, i))
+        for i in range(10, s._delay_sorry + 1):
+            bs.apptimer((i - 10) / 10, bs.Call(s._ok_sorry, i))
 
     def _send_gg(s):
-        if s._a != s._delay:
+        if s._a_gg != s._delay_gg:
             push("Too fast!")
             return
         bs.chatmessage(random.choice(gg_msgs))
-        for i in range(10, s._delay + 1):
-            bs.apptimer((i - 10) / 10, bs.Call(s._ok, i))
+        for i in range(10, s._delay_gg + 1):
+            bs.apptimer((i - 10) / 10, bs.Call(s._ok_gg, i))
 
 # ba_meta export babase.Plugin
 class byBordd(babase.Plugin):
     def __init__(s):
-        bauiv1lib.party.PartyWindow = PartyWindowWithTwoButtons
+        bauiv1lib.party.PartyWindow = PartyWindowWithTwoIndependentCooldowns
