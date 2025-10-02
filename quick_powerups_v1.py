006cdf49b7c619cdd4a8d25d7eb2f297da4c8f2f
RoboLearnAI47 <140956306+codesFolder@users.noreply.github.com>
2025-10-03 01:18:05 +0530

    Update quick_powerups_v1.py

--- quick_powerups_v1.py
+++ quick_powerups_v1.py
@@ -1,90 +1,62 @@
 # ba_meta require api 9
-# ba_meta export babase.Plugin
 
-from __future__ import annotations
-from typing import TYPE_CHECKING
 import babase as ba
 import bauiv1 as bui
+import bascenev1 as bs
 from bauiv1lib.ingamenu import InGameMenuWindow
 
-if TYPE_CHECKING:
-    from typing import Any, Optional
+# --- All functions are defined first, before the final plugin class ---
 
-# THIS IS THE MAIN PLUGIN CLASS.
-# IT MUST BE HERE, IMMEDIATELY AFTER THE '# ba_meta export' LINE.
-class WorkingGlovesPlugin(ba.Plugin):
-    def __init__(self) -> None:
-        # This function will modify the game's menu.
-        patch_in_game_menu()
-
-# --- All other functions and code are defined below the main plugin class ---
-
-def get_player_spaz() -> Optional[ba.Actor]:
-    """Finds the character actor for the player using this device."""
-    
-    # We need to get the current 'activity' (the running game or menu)
+def get_player_spaz():
+    """Finds the character of the local player."""
     activity = ba.get_foreground_host_activity()
-    if not activity:
-        return None
-
-    # Look through all players in the game.
-    for player in activity.players:
-        # Check if the player is alive and is the one using this instance of the game.
-        if player.is_alive() and player.sessionplayer.inputdevice.is_attached_to_player():
-            return player.actor
+    if activity:
+        for player in activity.players:
+            if player.is_alive() and player.sessionplayer.inputdevice.is_attached_to_player():
+                return player.actor
     return None
 
 def give_gloves() -> None:
-    """This is the simple function that runs when the button is clicked."""
-    
-    # Find the player's character in the game.
+    """The action for the button: find the player and give them gloves."""
     spaz = get_player_spaz()
-
-    # If we found the character and it's a valid character object...
     if spaz and hasattr(spaz, 'equip_boxing_gloves'):
-        # ...give them gloves.
         spaz.equip_boxing_gloves()
         ba.screenmessage("Gloves On!", color=(0, 1, 0))
         bui.getsound('powerup01').play()
     else:
-        # If not, show an error.
-        ba.screenmessage("Could not find you in the game!", color=(1, 0, 0))
+        ba.screenmessage("Could not find character!", color=(1, 0, 0))
         bui.getsound('error').play()
 
-def patch_in_game_menu() -> None:
-    """This function safely modifies the game's pause menu."""
-
-    # First, we store the original pause menu creation function.
-    _old_init = InGameMenuWindow.__init__
-
-    # Now, we create our new function that will replace the original.
-    def _new_init(self: InGameMenuWindow, **kwargs: Any) -> None:
+def patch_menu_to_add_button(original_function):
+    """This function takes the original menu code and adds our button to it."""
+    
+    def new_function(self, *args, **kwargs):
+        # First, run the original menu code.
+        original_function(self, *args, **kwargs)
         
-        # It's very important to call the original function first.
-        # This builds the normal pause menu.
-        _old_init(self, **kwargs)
-
-        # We must check if the menu was created successfully before adding our button.
-        if self._root_widget.exists() and self._leave_button.exists():
+        # Now, add our new button to the already-created menu.
+        bui.buttonwidget(
+            parent=self._root_widget,
+            size=(160, 50),
+            label='Get Gloves',
+            on_activate_call=give_gloves,
+            autoselect=True,
+            scale=1.1,
+            text_scale=1.2,
+            color=(0.8, 0.2, 0.2),
+            # Position it relative to the leave button to avoid conflicts.
+            position=(self._leave_button.get_position()[0], self._leave_button.get_position()[1] - 60)
+        )
             
-            # Get the position of the original 'Leave Game' button.
-            pos = self._leave_button.get_position()
+    return new_function
 
-            # Move the original 'Leave Game' button up by 50 units to make space.
-            bui.buttonwidget(edit=self._leave_button, position=(pos[0], pos[1] + 50))
+# ba_meta export babase.Plugin
+# This is the final and most important part.
+# The game looks for this exact line, and the class MUST come immediately after it.
+class FinalPlugin(ba.Plugin):
+    def __init__(self):
+        # Replace the game's menu function with our modified one.
+        InGameMenuWindow._refresh_in_game = patch_menu_to_add_button(
+            InGameMenuWindow._refresh_in_game
+        )
 
-            # Now, create our new 'Get Gloves' button in the empty space.
-            bui.buttonwidget(
-                parent=self._root_widget,
-                position=pos, # Use the original position.
-                size=(160, 50),
-                label='Get Gloves',
-                on_activate_call=give_gloves, # When clicked, run our simple function.
-                autoselect=True,
-                scale=1.1,
-                text_scale=1.2,
-                color=(0.8, 0.2, 0.2)
-            )
-
-    # Finally, we replace the game's original menu function with our new one.
-    InGameMenuWindow.__init__ = _new_init
